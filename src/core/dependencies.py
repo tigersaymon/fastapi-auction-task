ccde071467587c4c5b7777a83f6a4c9eb5609a08
Oleksandr Prymak <prymak.olexandr@lll.kpi.ua>
2026-03-01 00:14:09 +0200

    feat(ws): setup lifespan and dependencies for uow and websockets

--- src/core/dependencies.py
+++ src/core/dependencies.py
@@ -1,12 +1,19 @@
 from typing import Annotated
 
 from fastapi import Depends
+from starlette.requests import HTTPConnection
 
 from src.core.interfaces import AbstractUnitOfWork, SQLAlchemyUnitOfWork
+from ws.manager import ConnectionManager
 
 
 def get_uow() -> AbstractUnitOfWork:
     return SQLAlchemyUnitOfWork()
 
 
+def get_ws_manager(conn: HTTPConnection) -> ConnectionManager:
+    return conn.app.state.ws_manager
+
+
 UnitOfWorkDep = Annotated[AbstractUnitOfWork, Depends(get_uow)]
+ConnectionManagerDep = Annotated[ConnectionManager, Depends(get_ws_manager)]
