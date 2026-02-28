dcea42582a5c4070b1b528053b3f71a8d3444ce4
Oleksandr Prymak <prymak.olexandr@lll.kpi.ua>
2026-02-28 23:31:17 +0200

    fix(migrations): fix alembic enum bug

--- src/lots/models.py
+++ src/lots/models.py
@@ -1,11 +1,15 @@
 from datetime import UTC, datetime
 from decimal import Decimal
 from enum import StrEnum
+from typing import TYPE_CHECKING
 
 from sqlalchemy import DateTime, Enum, Numeric, String, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.core.database import Base
+
+if TYPE_CHECKING:
+    from src.bids.models import Bid
 
 
 class LotStatus(StrEnum):
@@ -14,7 +18,10 @@ class LotStatus(StrEnum):
 
 
 class Lot(Base):
-    __mapper_args__ = {"version_id_col": "version"}  # noqa: RUF012
+    # to avoid race condition
+    version: Mapped[int] = mapped_column(default=1)
+
+    __mapper_args__ = {"version_id_col": version}  # noqa: RUF012
 
     title: Mapped[str] = mapped_column(String(255))
     description: Mapped[str | None] = mapped_column(String(1024))
@@ -32,12 +39,9 @@ class Lot(Base):
         server_default=func.now(),
     )
 
-    version: Mapped[int] = mapped_column(default=1)
-
     # RELATIONS
-    bids: Mapped[list["Bid"]] = relationship(  # noqa: F821
+    bids: Mapped[list["Bid"]] = relationship(
         back_populates="lot",
-        lazy="selectin",
         order_by="Bid.created_at.desc()",
     )
 
