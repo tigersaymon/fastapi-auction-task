8067ba1272eeee21891647e6c1a0f226a315b238
Oleksandr Prymak <prymak.olexandr@lll.kpi.ua>
2026-03-01 17:59:43 +0200

    feat(deps): add auction service dependency

--- src/core/dependencies.py
+++ src/core/dependencies.py
@@ -4,7 +4,8 @@ from fastapi import Depends
 from starlette.requests import HTTPConnection
 
 from src.core.interfaces import AbstractUnitOfWork, SQLAlchemyUnitOfWork
-from ws.manager import ConnectionManager
+from src.lots.service import AuctionService
+from src.ws.manager import ConnectionManager
 
 
 def get_uow() -> AbstractUnitOfWork:
@@ -15,5 +16,13 @@ def get_ws_manager(conn: HTTPConnection) -> ConnectionManager:
     return conn.app.state.ws_manager
 
 
+def get_auction_service(
+    uow: Annotated[AbstractUnitOfWork, Depends(get_uow)],
+    ws_manager: Annotated[ConnectionManager, Depends(get_ws_manager)],
+) -> AuctionService:
+    return AuctionService(uow=uow, ws_manager=ws_manager)
+
+
 UnitOfWorkDep = Annotated[AbstractUnitOfWork, Depends(get_uow)]
 ConnectionManagerDep = Annotated[ConnectionManager, Depends(get_ws_manager)]
+AuctionServiceDep = Annotated[AuctionService, Depends(get_auction_service)]
